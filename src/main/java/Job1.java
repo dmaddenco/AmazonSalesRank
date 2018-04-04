3e5d5b8b76a04dec82daf3c64a9c0a57a48e8324
David Madden <dmaddenco@gmail.com>
2018-04-03 22:09:03 -0600

    updated project

---
+++ src/main/java/Job1.java
@@ -0,0 +1,60 @@
+/**
+ * Created by dmadden on 2/20/18.
+ */
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+class Job1 {
+  static class Job1Mapper extends Mapper<Object, Text, PA2.DocIdUniComKey, IntWritable> {
+    private final static IntWritable one = new IntWritable(1);
+    private final Text word = new Text();
+    private final IntWritable docID = new IntWritable();
+    private PA2.DocIdUniComKey comKey = new PA2.DocIdUniComKey();
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+      String values[] = value.toString().split("<====>");
+
+      if (values.length >= 3) {
+        String id = values[1];
+        String article = values[2];
+        String[] sentences = article.split("\\.");
+
+        for (String sentence : sentences) {
+          if (!sentence.equals("")) {
+            StringTokenizer itrWord = new StringTokenizer(sentence);
+
+            while (itrWord.hasMoreTokens()) {
+              String unigram = itrWord.nextToken().toLowerCase().replaceAll("[^a-z0-9 ]", "");
+              word.set(unigram);
+              docID.set(Integer.parseInt(id));
+              comKey = new PA2.DocIdUniComKey(docID, word);
+              context.write(comKey, one);
+            }
+
+          }
+        }
+      }
+    }
+  }
+
+  static class Job1Reducer extends Reducer<PA2.DocIdUniComKey, IntWritable, PA2.DocIdUniComKey, IntWritable> {
+    private final IntWritable result = new IntWritable();
+
+    public void reduce(PA2.DocIdUniComKey key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+      int sum = 0;
+
+      for (IntWritable val : values) {
+        sum += val.get();
+      }
+
+      result.set(sum);
+      context.write(key, result);
+    }
+  }
+}
---
+++ src/main/java/PA2.java
@@ -0,0 +1,196 @@
+/**
+ * Created by dmadden on 2/20/18.
+ */
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.PathFilter;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.mapreduce.Counter;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Partitioner;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class PA2 {
+
+  public static class CountersClass {
+    public enum N_COUNTERS {
+      SOMECOUNT
+    }
+  }
+
+  private static class PartitionerInitial extends Partitioner<DocIdUniComKey, IntWritable> {
+    public int getPartition(DocIdUniComKey key, IntWritable value, int numReduceTasks) {
+      return Math.abs(key.getDocID().hashCode() % numReduceTasks);
+    }
+  }
+
+  public static class DocIdUniComKey implements Writable, WritableComparable<DocIdUniComKey> {
+    private IntWritable docID = new IntWritable();
+    private Text unigram = new Text();
+
+    DocIdUniComKey() {
+      this.docID = new IntWritable();
+      this.unigram = new Text();
+    }
+
+    DocIdUniComKey(IntWritable id, Text uni) {
+      this.docID.set(Integer.parseInt(id.toString()));
+      this.unigram.set(uni);
+    }
+
+    public void write(DataOutput out) throws IOException {
+      this.docID.write(out);
+      this.unigram.write(out);
+    }
+
+    public void readFields(DataInput in) throws IOException {
+      this.docID = new IntWritable();
+      this.unigram = new Text();
+      this.docID.readFields(in);
+      this.unigram.readFields(in);
+    }
+
+    IntWritable getDocID() {
+      return this.docID;
+    }
+
+    Text getUnigram() {
+      return this.unigram;
+    }
+
+    public int compareTo(DocIdUniComKey pair) {
+      int compareValue = this.docID.compareTo(pair.getDocID());
+      if (compareValue == 0) {
+        compareValue = unigram.compareTo(pair.getUnigram());
+      }
+      return -1 * compareValue; //descending order
+    }
+
+    @Override
+    public String toString() {
+      return docID.toString() + "\t" + unigram.toString();
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    conf.set("mapred.textoutputformat.separator", "\t");
+    int numReduceTask = 32;
+
+    Path inputPath = new Path(args[0]);
+    Path outputPathTemp1 = new Path(args[1] + "Temp1");
+    Path outputPathTemp2 = new Path(args[1] + "Temp2");
+    Path outputPathTemp3 = new Path(args[1] + "Temp3");
+    Path outputPathTemp4 = new Path(args[1] + "Temp4");
+    Path outputPath = new Path(args[1]);
+
+    Job job1 = Job.getInstance(conf, "pa2_job1");
+    Job job2 = Job.getInstance(conf, "pa2_job2");
+    Job job3 = Job.getInstance(conf, "pa2_job3");
+    Job job4 = Job.getInstance(conf, "pa2_job4");
+    Job job5 = Job.getInstance(conf, "pa2_job5");
+
+    job1.setJarByClass(PA2.class);
+    job1.setNumReduceTasks(numReduceTask);
+    job1.setPartitionerClass(PartitionerInitial.class);
+
+    job1.setMapperClass(Job1.Job1Mapper.class);
+    job1.setReducerClass(Job1.Job1Reducer.class);
+    job1.setOutputKeyClass(DocIdUniComKey.class);
+    job1.setOutputValueClass(IntWritable.class);
+
+    FileInputFormat.addInputPath(job1, inputPath);
+    FileOutputFormat.setOutputPath(job1, outputPathTemp1);
+
+    if (job1.waitForCompletion(true)) {
+      job2.setJarByClass(PA2.class);
+      job2.setNumReduceTasks(numReduceTask);
+
+      job2.setMapperClass(Job2.Job2Mapper.class);
+      job2.setReducerClass(Job2.Job2Reducer.class);
+
+      job2.setMapOutputKeyClass(IntWritable.class);
+      job2.setMapOutputValueClass(Text.class);
+      job2.setOutputKeyClass(IntWritable.class);
+      job2.setOutputValueClass(Text.class);
+
+      FileInputFormat.addInputPath(job2, outputPathTemp1);
+      FileOutputFormat.setOutputPath(job2, outputPathTemp2);
+
+      if (job2.waitForCompletion(true)) {
+    	Counter count = job2.getCounters().findCounter(CountersClass.N_COUNTERS.SOMECOUNT);
+          
+        job3.setJarByClass(PA2.class);
+        job3.setNumReduceTasks(numReduceTask);
+
+        job3.setMapperClass(Job3.Job3Mapper.class);
+        job3.setReducerClass(Job3.Job3Reducer.class);
+
+        job3.setMapOutputKeyClass(Text.class);
+        job3.setMapOutputValueClass(Text.class);
+        job3.setOutputKeyClass(Text.class);
+        job3.setOutputValueClass(Text.class);
+
+        FileInputFormat.addInputPath(job3, outputPathTemp2);
+        FileOutputFormat.setOutputPath(job3, outputPathTemp3);
+
+        if (job3.waitForCompletion(true)) {
+          job4.getConfiguration().setLong(CountersClass.N_COUNTERS.SOMECOUNT.name(), count.getValue());
+
+          job4.setJarByClass(PA2.class);
+          job4.setNumReduceTasks(numReduceTask);
+
+          job4.setMapperClass(Job4.Job4Mapper.class);
+          job4.setReducerClass(Job4.Job4Reducer.class);
+
+          job4.setMapOutputKeyClass(IntWritable.class);
+          job4.setMapOutputValueClass(Text.class);
+          job4.setOutputKeyClass(IntWritable.class);
+          job4.setOutputValueClass(Text.class);
+
+          FileInputFormat.addInputPath(job4, outputPathTemp3);
+          FileOutputFormat.setOutputPath(job4, outputPathTemp4);
+
+          if (job4.waitForCompletion(true)) {
+            FileSystem fs = FileSystem.get(conf);
+            FileStatus[] fileList = fs.listStatus((outputPathTemp4),
+                    new PathFilter() {
+                      public boolean accept(Path path) {
+                        return path.getName().startsWith("part-");
+                      }
+                    });
+            for (FileStatus aFileList : fileList) {
+              job5.addCacheFile((aFileList.getPath().toUri()));
+            }
+
+            job5.setJarByClass(PA2.class);
+            job5.setNumReduceTasks(numReduceTask);
+
+            job5.setMapperClass(Job5.Job5Mapper.class);
+            job5.setReducerClass(Job5.Job5Reducer.class);
+
+            job5.setMapOutputKeyClass(IntWritable.class);
+            job5.setMapOutputValueClass(Text.class);
+            job5.setOutputKeyClass(IntWritable.class);
+            job5.setOutputValueClass(Text.class);
+
+            FileInputFormat.addInputPath(job5, inputPath);
+            FileOutputFormat.setOutputPath(job5, outputPath);
+            System.exit(job5.waitForCompletion(true) ? 0 : 1);
+          }
+        }
+      }
+    }
+  }
+}
