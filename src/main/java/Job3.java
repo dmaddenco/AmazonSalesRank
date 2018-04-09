6612eae631ad337088ee06f073e70e81789601c9
David Madden <dmaddenco@gmail.com>
2018-04-09 14:37:27 -0600

    job3 logic done, need to test against data

--- src/main/java/Job3.java
+++ src/main/java/Job3.java
@@ -5,63 +5,78 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 class Job3 {
   /**
-   * Map output from previous MapReduce job to new < key, value > pair
+   * Map output from previous MapReduce job < {asin \t unigram}, {frequency \t salesRank} > to new < key, value > pair
+   *
    * @param LongWritable object that can be ignored
-   * @param Text object that contains all the output from previous MapReduce job
-   * @return Key value pair < unigram, {docId \t TFvalue} >
+   * @param Text         object that contains all the output from previous MapReduce job
+   * @return Key value pair < asin, {unigram \t frequency \t salesRank} >
    */
   static class Job3Mapper extends Mapper<LongWritable, Text, Text, Text> {
-    private final Text unigramKey = new Text();
+    private final Text compKey = new Text();
     private final Text compValue = new Text();
 
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-      String[] inputArray = value.toString().split("\t");
-      String docId = inputArray[0];
-      String uni = inputArray[1];
-      String freq = inputArray[2];
-      String tf = inputArray[3];
+      Set<String> uniqueASIN = new HashSet<String>();
+      String[] keyArray = key.toString().split("\t");
+      String[] valueArray = value.toString().split("\t");
+      String asin = keyArray[0];
+      String unigram = keyArray[0];
+      String frequency = valueArray[0];
+      String salesRank = valueArray[1];
 
-      unigramKey.set(uni);
-      compValue.set(docId + "\t" + tf);
-      context.write(unigramKey, compValue);
+      compKey.set(asin);
+      compValue.set(unigram + "\t" + frequency + "\t" + salesRank);
+      context.write(compKey, compValue);
+
+      if (!uniqueASIN.contains(asin)){
+        uniqueASIN.add(asin);
+        context.getCounter(Driver.CountersClass.N_COUNTERS.SOMECOUNT).increment(1); //Increment the counter
+      }
     }
   }
 
   /**
-   * Calculate IDF value
-   * @param Text object key that is the unigram
-   * @param Text object value that is composite value of {docId \t TFvalue}
-   * @return Write to context the < key, value > pair of < unigram, {docId \t TFvalue \t Ni} >
+   * Calculate TF value
+   *
+   * @param Text object key that is the asin
+   * @param Text object value that is composite value of {unigram \t frequency \t salesRank}
+   * @return Write to context the < key, value > pair of < asin, {unigram \t frequency \t TFvalue \t salesRank} >
    */
   static class Job3Reducer extends Reducer<Text, Text, Text, Text> {
-    private final Text unigramKey = new Text();
+    private final Text compKey = new Text();
     private final Text compValue = new Text();
 
     public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
       ArrayList<String> valuesCopy = new ArrayList<String>();
-      double ni;
-      String tempValue;
+      double maxFreq = 0;
+      double tf;
 
-      //create deep copy of values
       for (Text val : values) {
         valuesCopy.add(val.toString());
       }
 
-      //number of documents where unigram exists
-      ni = valuesCopy.size();
-
-      //iterate over ArrayList of value copies
       for (String val : valuesCopy) {
-        String[] inputArray = val.split("\t");
-        String docId = inputArray[0];
-        String tf = inputArray[1];
-        tempValue = docId + "\t" + tf + "\t" + ni;
-        unigramKey.set(key.toString());
-        compValue.set(tempValue);
-        context.write(unigramKey, compValue);
+        String[] valuesSplit = val.split("\t");
+        int frequency = Integer.parseInt(valuesSplit[1]);
+        if (frequency > maxFreq) {
+          maxFreq = frequency;
+        }
+      }
+
+      for (String val : valuesCopy) {
+        String[] valuesSplit = val.split("\t");
+        String unigram = valuesSplit[0];
+        int frequency = Integer.parseInt(valuesSplit[1]);
+        String salesRank = valuesSplit[2];
+        tf = 0.5 + 0.5 * (frequency / maxFreq);
+        compKey.set(key);
+        compValue.set(unigram + "\t" + frequency + "\t" + tf + "\t" + salesRank);
+        context.write(compKey, compValue);
       }
     }
   }
