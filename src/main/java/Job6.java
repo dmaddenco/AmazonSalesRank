ecb560467b1750f42468527d59094872a9a9eab4
David Madden <dmaddenco@gmail.com>
2018-04-13 11:13:56 -0600

    testing package

--- src/main/java/Job6.java
+++ src/main/java/Job6.java
@@ -1,10 +1,11 @@
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 //import org.json.JSONObject;
 
 import java.io.BufferedReader;
