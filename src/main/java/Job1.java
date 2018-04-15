69a684c0f16bb62b0b9ebff6914db6752829c642
Andy Thai <athai@neptune.cs.colostate.edu>
2018-04-15 12:54:41 -0600

    Removed job0 and finished parsing data

--- src/main/java/Job1.java
+++ src/main/java/Job1.java
@@ -2,67 +2,106 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-
+//import org.json.simple.*;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.HashMap;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
 class Job1 {
   /**
-   * Maps text read from input file to < key, value > pairs of < {unigram \t frequecy}, 1 >
-   * @param Object is the dummy key created when reading file
-   * @param Text contains string objects that are from the input file
-   * @return Writes to context the < DocIdUniComKey, 1 > key value pair
+   * Parses metadata for asin and sales rank
+   * @param asin parsed product asin number 
+   * @param rank parsed product sales rank number
+   * @return (asin,rank)
    */
-  static class Job1Mapper extends Mapper<Object, Text, Driver.DocIdUniComKey, IntWritable> {
-    private final static IntWritable one = new IntWritable(1);
-    private final Text word = new Text();
-    private final IntWritable docID = new IntWritable();
-    private Driver.DocIdUniComKey comKey = new Driver.DocIdUniComKey();
+  public static class Job0Mapper extends Mapper<Object, Text, Text, Text>{
+        //private final static DocComposite doc = new DocComposite();
+        private final static Text asin = new Text();
+        private final static Text rank = new Text();
+        
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            String meta = value.toString();
+            if (!meta.isEmpty()) {
+                if(meta.contains("asin") && meta.contains("'salesRank': {'Electronics':")){
+
+                    StringTokenizer itrWord = new StringTokenizer(meta);
+                    
+                    while (itrWord.hasMoreTokens()) {
+                        String unigram = itrWord.nextToken().toLowerCase().replaceAll("[^A-Za-z0-9]","");
+                        if(unigram.equals("asin")){
+                            String num = itrWord.nextToken().toLowerCase().replaceAll("[^A-Za-z0-9]","");
+                            asin.set(num);
+                        }
+                        if(unigram.equals("salesrank")){
+                            String temp = itrWord.nextToken();
+                            String electronicText = itrWord.nextToken().toLowerCase().replaceAll("[^A-Za-z0-9]","");
+                            rank.set(electronicText);
+                        }
+                    }
+                    context.write(asin,rank);
+                }
+            }
+        }
+    }
+  
+  /**
+   * Parses customer reviews data for asin and review text
+   * @param asin parsed product asin number 
+   * @param data parsed product's review text
+   * @return (asin,reviewText)
+   */
+  static class Job1Mapper extends Mapper<Object, Text, Text, Text> {
+    private final static Text asin = new Text();
+    private final static Text data = new Text();
 
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-      String values[] = value.toString().split("<====>");
-
-      if (values.length >= 3) {
-        String id = values[1];
-        String article = values[2];
-        String[] sentences = article.split("\\.");
-
-        for (String sentence : sentences) {
-          if (!sentence.equals("")) {
-            StringTokenizer itrWord = new StringTokenizer(sentence);
-
-            while (itrWord.hasMoreTokens()) {
-              String unigram = itrWord.nextToken().toLowerCase().replaceAll("[^a-z0-9 ]", "");
-              word.set(unigram);
-              docID.set(Integer.parseInt(id));
-              comKey = new Driver.DocIdUniComKey(docID, word);
-              context.write(comKey, one);
-            }
-
-          }
-        }
+      String reviews = value.toString();
+      if(!reviews.isEmpty()){
+        HashMap<String,Object> map = new ObjectMapper().readValue(reviews,HashMap.class);
+        String tempAsin = map.get("asin").toString();
+        String reviewText = map.get("reviewText").toString();
+        
+        asin.set(tempAsin);
+        data.set(reviewText);
+        context.write(asin,data);
+        
       }
     }
   }
+  
 
   /**
-   * Sums up all counts for unique key, in this case a unigram
-   * @param DocIdUniComKey unique composite key that contains the {docId, unigram}
-   * @param IntWritable value of 1, for 1 occurrence for the composite key
-   * @return Writes to context the frequency of a unigram < unigram, frequency >
+   * Joins customer reviews and sales rank on asin key
+   * @param result value of review text and rank stored as text object 
+   * @param combo value of review text and rank stored as string object
+   * @return (asin,{reviewText,rank})
    */
-  static class Job1Reducer extends Reducer<Driver.DocIdUniComKey, IntWritable, Driver.DocIdUniComKey, IntWritable> {
-    private final IntWritable result = new IntWritable();
-
-    public void reduce(Driver.DocIdUniComKey key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-      int sum = 0;
-
-      for (IntWritable val : values) {
-        sum += val.get();
+  static class Job1Reducer extends Reducer<Text, Text, Text, Text> {
+    private final Text result = new Text();
+    private String combo = "";
+    
+    public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+      
+      String rank = "";
+      String reviewText = "";
+      for(Text val: values){
+        String temp = val.toString();
+        char myChar = temp.charAt(0);
+        
+        if(Character.isDigit(myChar)){
+            rank = temp;
+        }
+        else{
+            reviewText = temp;
+        }
       }
-
-      result.set(sum);
-      context.write(key, result);
+       if(!reviewText.isEmpty() && !rank.isEmpty()){ 
+        combo =  reviewText + "\t" + rank;
+        result.set(combo);
+        context.write(key, result);
+       }
     }
   }
 }
